5f0c793cb73ef9535cd1f53a432dce0e13a48611
Jack12xl <jack12xieling@163.com>
2020-03-23 17:18:56 +0800

    add previous change

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ from train_single_DRGAN import train_single_DRGAN
 from train_multiple_DRGAN import train_multiple_DRGAN
 from Generate_Image import Generate_Image
 import pdb
+from tqdm import tqdm
+from skimage import io, transform
 
 
 def DataLoader(data_place):
@@ -51,6 +53,114 @@ def DataLoader(data_place):
     return [images, id_labels, pose_labels, Nd, Np, Nz, channel_num]
 
 
+class Resize(object):
+    #  assume image  as H x W x C numpy array
+    def __init__(self, output_size):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+
+    def __call__(self, image):
+        h, w = image.shape[:2]
+        if h > w:
+            new_h, new_w = self.output_size, int(self.output_size * w / h)
+        else:
+            new_h, new_w = int(self.output_size * h / w), self.output_size
+
+        resized_image = transform.resize(image, (new_h, new_w))
+
+        if h > w:
+            diff = self.output_size - new_w
+            if diff % 2 == 0:
+                pad_l = int(diff / 2)
+                pad_s = int(diff / 2)
+            else:
+                pad_l = int(diff / 2) + 1
+                pad_s = int(diff / 2)
+
+            padded_image = np.lib.pad(resized_image, ((0, 0), (pad_l, pad_s), (0, 0)), 'edge')
+
+        else:
+            diff = self.output_size - new_h
+            if diff % 2 == 0:
+                pad_l = int(diff / 2)
+                pad_s = int(diff / 2)
+            else:
+                pad_l = int(diff / 2) + 1
+                pad_s = int(diff / 2)
+
+            padded_image = np.lib.pad(resized_image, ((pad_l, pad_s), (0, 0), (0, 0)), 'edge')
+
+        return padded_image
+
+def FetchData(data_place:str) -> list :
+    image_dir = os.path.join(data_place, "Images")
+
+    rsz = Resize(110)
+
+    Indv_dir = []
+    for x in os.listdir(image_dir):
+        if os.path.isdir(os.path.join(image_dir, x)):
+            Indv_dir.append(x)
+
+    Indv_dir = np.sort(Indv_dir)
+
+    images = np.zeros((7000, 110, 110, 3))
+    id_labels = np.zeros(7000)
+    pose_labels = np.zeros(7000)
+    count = 0
+    gray_count = 0
+
+    for i in tqdm(range(len(Indv_dir))):
+        Frontal_dir = os.path.join(image_dir, Indv_dir[i], 'frontal')
+        Profile_dir = os.path.join(image_dir, Indv_dir[i], 'profile')
+
+        front_img_files = os.listdir(Frontal_dir)
+        prof_img_files = os.listdir(Profile_dir)
+
+        for img_file in front_img_files:
+            img = io.imread(os.path.join(Frontal_dir, img_file))
+            if len(img.shape) == 2:
+                gray_count = gray_count + 1
+                continue
+            img_rsz = rsz(img)
+            images[count] = img_rsz
+            id_labels[count] = i
+            pose_labels[count] = 0
+            count = count + 1
+
+        for img_file in prof_img_files:
+            img = io.imread(os.path.join(Profile_dir, img_file))
+            if len(img.shape) == 2:
+                gray_count = gray_count + 1
+                continue
+            img_rsz = rsz(img)
+            images[count] = img_rsz
+            id_labels[count] = i
+            pose_labels[count] = 1
+            count = count + 1
+
+    id_labels = id_labels.astype('int64')
+    pose_labels = pose_labels.astype('int64')
+
+    # [0,255] -> [-1,1]
+    images = images * 2 - 1
+    # RGB -> BGR
+    images = images[:, :, :, [2, 1, 0]]
+    # B x H x W x C-> B x C x H x W
+    images = images.transpose(0, 3, 1, 2)
+
+    # 白黒画像データを取り除く
+    images = images[:gray_count * -1]
+    id_labels = id_labels[:gray_count * -1]
+    pose_labels = pose_labels[:gray_count * -1]
+    Np = int(pose_labels.max() + 1)
+    Nd = int(id_labels.max() + 1)
+    Nz = 50
+    channel_num = 3
+
+    print(images, id_labels, pose_labels, Nd, Np, Nz, channel_num)
+    return [images, id_labels, pose_labels, Nd, Np, Nz, channel_num]
+
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='DR_GAN')
@@ -62,10 +172,10 @@ if __name__=="__main__":
     parser.add_argument('-batch-size', type=int, default=8, help='batch size for training [default: 8]')
     parser.add_argument('-save-dir', type=str, default='snapshot', help='where to save the snapshot')
     parser.add_argument('-save-freq', type=int, default=1, help='save learned model for every "-save-freq" epoch')
-    parser.add_argument('-cuda', action='store_true', default=False, help='enable the gpu')
+    parser.add_argument('-cuda', action='store_true', default=True, help='enable the gpu')
     # data souce
     parser.add_argument('-random', action='store_true', default=False, help='use randomely created data to run program')
-    parser.add_argument('-data-place', type=str, default='./data', help='prepared data path to run program')
+    parser.add_argument('-data-place', type=str, default='./cfp-dataset/Data', help='prepared data path to run program')
     # model
     parser.add_argument('-multi-DRGAN', action='store_true', default=False, help='use multi image DR_GAN model')
     parser.add_argument('-images-perID', type=int, default=0, help='number of images per person to input to multi image DR_GAN')
@@ -96,10 +206,11 @@ if __name__=="__main__":
         images, id_labels, pose_labels, Nd, Np, Nz, channel_num = create_randomdata()
     else:
         print('n\Loading data from [%s]...' % args.data_place)
-        try:
-            images, id_labels, pose_labels, Nd, Np, Nz, channel_num = DataLoader(args.data_place)
-        except:
-            print("Sorry, failed to load data")
+        # try:
+        images, id_labels, pose_labels, Nd, Np, Nz, channel_num = FetchData(args.data_place)
+
+        # except:
+        #     print("Sorry, failed to load data")
 
     # model
     if args.snapshot is None:
